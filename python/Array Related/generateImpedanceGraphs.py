129e05f2ff3543d3dfb01d86356d5b9482769f98
Winthrop Gillis <wgillis@bu.edu>
2014-07-24 20:17:27 -0400

    Added variability to folder zipping and a new function parameter to sendEmail

--- python/Array Related/generateImpedanceGraphs.py
+++ python/Array Related/generateImpedanceGraphs.py
@@ -3,36 +3,6 @@
 Spyder Editor
 
 Built by Winthrop Gillis 7.11.2014
-"""
-# change this to True when trying to imporove upon the code
-DEBUG = True
-
-
-import os
-
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-
-#import numpy as np # no need for numpy
-
-# plotting library
-import matplotlib.pyplot as plt
-
-# data management library
-import pandas as pd
-
-# utility libraries
-import glob, datetime, shutil, zipfile, smtplib
-
-# classes to build an email message
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEBase import MIMEBase
-from email.MIMEText import MIMEText
-from email import Encoders
-
-# contains the email and password to the gardner lab gmail account
-from emailVars import em, passw
 
 #This script runs in the current directory. It runs through all the files 
 #in the current directory, and reads in files that end in the .txt
@@ -61,39 +31,72 @@ from emailVars import em, passw
 #
 
 # TODO:
-#     - add a function to email the attachment automatically
 #     - add a figure of average channel values and std
 #     - comment code so that others know how it works
+"""
+# change this to True when trying to imporove upon the code
+DEBUG = True
 
+import os
 
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+#import numpy as np # no need for numpy
+
+# plotting library
+import matplotlib.pyplot as plt
+# data management library
+import pandas as pd
+# utility libraries
+import glob, datetime, shutil, zipfile, smtplib
+# classes to build an email message
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+# contains the email and password to the gardner lab gmail account
+from emailVars import em, passw
 
 
 OUTPUT_FOLDER = 'processedData'
+
 today = datetime.datetime.today()
+# my sneaky way of figuring out which day monday is
 monday = today - datetime.timedelta(today.weekday())
+# add 4 days to get friday
 friday = monday + datetime.timedelta(4)
+# name of the folder containing this week's data
 weekFolder = monday.date().isoformat() + ' to ' + friday.date().isoformat()
 
-
+# make sure this folder already exists, and create it if it doesn't
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
-
+# this will be the folder path to put images and text files into
 imagePath = os.path.join(OUTPUT_FOLDER, weekFolder)
-
+# this will almost always make a new folder for the data, unless you run this code 
+# twice in one week
 if not os.path.exists(imagePath):
     os.makedirs(imagePath)
-
+# figures and text data folders will be made here
 figPath = os.path.join(imagePath, 'figures')
 os.makedirs(figPath)
 txtPath = os.path.join(imagePath, 'textFileData')
 os.makedirs(txtPath)
 
 def readFile(filename):
+    ''' this reads in the text file as a csv with the tab key as the delimiter
+    it also skips the starting lines of the file
+    it uses the first column as the index
+    '''
     df = pd.read_csv(filename, sep='\t', skiprows=[0,1,20], index_col=0,)
+    # this gets the name of each column
     names = df.keys()
-
+    # this removes the third column, which contains data about the phase
     df = df.drop(names[-1], 1)
     names = names[:-1]
+
     #avgImp = df[names[0]].mean()
     #avgPhs = df[names[1]].mean()
     #df = df.append({names[0]: avgImp, names[1]: avgPhs}, ignore_index=True)
@@ -108,73 +111,119 @@ def importFiles():
     Returns a list of each file dataframe
     """
     # glob module provides easy filename handling
+    # get all files that end in .txt
     files = glob.glob('*.txt')
-    #print files 
-    return [readFile(f) for f in files], files
+    if files:
+        #print files 
+        # return all the data and the file names
+        return [readFile(f) for f in files], files
+    else:
+        return None, None
 
 def plotFig(dataFrame, filename, number):
+    '''
+    This function plots a graph for every file in the folder
+    '''
+    # get the column names
     demKeys = dataFrame.keys()
+    # create a new dataframe (how the data is represented) with only the first set of datapoints
     dataFrame = pd.DataFrame(data=dataFrame[demKeys[0]], index=dataFrame.index)
-
+    # returns a list of datapoints that have impedance values below 5 MOhms
     lessThan5 = [x for x in dataFrame[demKeys[0]] if x < 5]
+    # average impedances for fibers with less than 5 MOhm impedances
     smallAvg = sum(lessThan5)/ len(lessThan5)
     
+    # actually plots the data
     ax = dataFrame.plot(kind='bar', title = files[number][:-4] + ' - ' + demKeys[0], legend=False, ylim=[0,3], color='r')
     #print dataFrame.std()
+    # add informative text onto the figure
     ax.text(len(dataFrame.index) - 7, 2.8, 'Std: ' + str(dataFrame.std().item())[:5], fontsize=13)
     ax.text(len(dataFrame.index) - 7, 2.6, 'Average: ' + str(dataFrame.mean().item())[:5], fontsize=13)
     ax.text(len(dataFrame.index) - 10, 2.4 , 'Avg of channels w/ <5MOhms: ' + str(smallAvg)[:5], fontsize=13)
+    # axes labels
     ax.set_xlabel('Channels')
     ax.set_ylabel(demKeys[0])
 
-
+    # saves the figure
     fig = ax.get_figure()
-
     fPath = os.path.join(figPath, '{fName} - fig {num}.png'.format(fName = filename[:-4], num = number))
-
     fig.savefig(fPath)
     
 def moveTextFiles():
+    '''
+    This function just moves all text files from the current directory
+    to a folder located in the current week's folder
+    '''
     files = glob.glob('*.txt')
     for f in files:
         shutil.move(f, os.path.join(txtPath, f))
 
 def zipdir(path):
+    '''
+    This function creates a zip file of the compressed data
+    '''
     with zipfile.ZipFile(path + '.zip', 'w') as z:
         for root, dirs, files in os.walk(path):
             for f in files:
                 z.write(os.path.join(root, f))
     
-def sendEmail(recipient):
-    msg = MIMEMultipart()
-    msg['Subject'] = 'Zip folder test'
-    msg['To'] = recipient
-    msg['From'] = em
-    msg.attach(MIMEText('Here is the data for this week: '))
-    attach = MIMEBase('application', 'zip')
-    with open(imagePath + '.zip', 'rb') as r:
-        attach.set_payload(r.read())
-    Encoders.encode_base64(attach)
-    attach.add_header('Content-Disposition', 'attachment', filename='2014-07-14 to 2014-07-18.zip')
-    msg.attach(attach)
-    s = smtplib.SMTP('smtp.gmail.com:587') #465
-    s.starttls()
-    s.login(em, passw)
-    s.sendmail(em, recipient, msg.as_string())
-    s.quit()
+def sendEmail(recipient, empty=False):
+    '''
+    Sends an email to the sepcified recipient.
+    If there are no files present then an email will be sent out
+    saying no impedances were tested this week
+    '''
+    if not empty:
+        msg = MIMEMultipart()
+        # Set subject of the message; using a global value
+        msg['Subject'] = 'Impedance Values for the week of' + weekFolder
+        msg['To'] = recipient
+        # using a global value
+        msg['From'] = em
+        msg.attach(MIMEText('This is an automated script built by Win\nAttached are the array impedance values for this week: '))
+        attach = MIMEBase('application', 'zip')
+        with open(imagePath + '.zip', 'rb') as r:
+            attach.set_payload(r.read())
+        Encoders.encode_base64(attach)
+        attach.add_header('Content-Disposition', 'attachment', filename=weekFolder + '.zip')
+        msg.attach(attach)
+        s = smtplib.SMTP('smtp.gmail.com:587') #465
+        s.starttls()
+        s.login(em, passw)
+        s.sendmail(em, recipient, msg.as_string())
+        s.quit()
+    else:
+        msg = MIMEText('No arrays were impedance tested this week. (Or at least their text files weren\'t put into this folder.')
+        msg['Subject'] = 'No impedance values this week'
+        msg['To'] = recipient
+        msg['From'] = em
+        s = smtplib.SMTP('smtp.gmail.com:587')
+        s.starttls()
+        s.login(em, passw)
+        s.sendmail(em, recipient, msg.as_string())
+        s.quit()
 
 
 dataFrames, files = importFiles()
 
-for index, (frame, fil) in enumerate(zip(dataFrames, files)):
-    plotFig(frame, fil, index)
+# do the following steps only if the files are present
+if files:
 
-moveTextFiles()
+    for index, (frame, fil) in enumerate(zip(dataFrames, files)):
+        plotFig(frame, fil, index)
 
-zipdir(imagePath)
+    moveTextFiles()
 
+    zipdir(imagePath)
 
-sendEmail('wgillis@bu.edu')
+    if DEBUG:
+        sendEmail('wgillis@bu.edu')
 
-if not DEBUG:
-    sendEmail('timothyg@bu.edu')
+    else:
+        sendEmail('timothyg@bu.edu')
+else:
+    if DEBUG:
+        sendEmail('wgillis@bu.edu', empty = True)
+    else: 
+        sendEmail('timothyg@bu.edu', empty = True)
+    # send an email saying there were no arrays that were tested in this week
